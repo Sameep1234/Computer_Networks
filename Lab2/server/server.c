f67ad24f898ee7ab86b866c5ebd437d58c175edf
kairavishah2001 <kairavishah2001@gmail.com>
2022-03-16 18:46:52 +0530

    GET condition and error function modified

--- Lab2/server/server.c
+++ Lab2/server/server.c
@@ -12,10 +12,10 @@
 #define SERVER_PORT 8080
 #define MAX_LINE BUFSIZ
 
-void error_handler()
+void error_handler(char *error_msg)
 {
-    perror("error");
-    exit(1);
+    perror(error_msg);
+    exit(EXIT_FAILURE);
 }
 
 int main()
@@ -39,7 +39,7 @@ int main()
     s = socket(PF_INET, SOCK_DGRAM, 0);
     if (s < 0)
     {
-        error_handler();
+        error_handler("socket creation failure");
     }
     puts("Socket created");
 
@@ -47,35 +47,36 @@ int main()
     b = bind(s, (struct sockaddr *)&sin, sizeof(sin));
     if (b < 0)
     {
-        error_handler();
+        error_handler("bind error");
     }
     puts("Bind done");
-    int bytes_read = 0, total_bytes, loop_count = 0;
 
     while (1)
     {
+        int bytes_read = 0, total_bytes, loop_count = 0;
 
-        while ((len = recvfrom(s, buf, MAX_LINE, 0, (struct sockaddr *)&clientaddr, (socklen_t *)&clientaddr_len)))
+        while ((len = recvfrom(s, buf, MAX_LINE, 0, (struct sockaddr *)&clientaddr, (socklen_t *)&clientaddr_len)) > 0)
         {
-            if (strcmp(buf, "GET") == 0)
+            if (buf[0] == 'G' && buf[1] == 'E' && buf[2] == 'T')
             {
                 printf("%s\n", buf); // "GET" coming or not!
                 /* Open the file that we wish to transfer */
+                bzero(buf, sizeof(buf));
 
                 fp = fopen(fileName, "rb");
                 if (fp == NULL)
                 {
-                    error_handler();
+                    error_handler("Opening file failed!");
                 }
 
                 /* Read data from file and send it */
-                bzero(buf, sizeof(buf));
+                //bzero(buf, sizeof(buf));
 
                 while (!feof(fp)) // Continue the loop until all the bytes of the file is read.
                 {
                     struct timespec t;
                     printf("Here1\n");
-                    t.tv_sec = 1;
+                    t.tv_sec = 0.1;
                     printf("Here2\n");
                     nanosleep((const struct timespec *)&t, NULL);
                     loop_count++;
@@ -84,32 +85,34 @@ int main()
                     // printf("%s\n", buf);
                     if (ferror(fp) != 0)
                     {
-                        error_handler();
+                        error_handler("error in reading file");
                     }
                     printf("Bytes Send: %d\n", bytes_read);
                     // printf("BUF CONTENTS: %s\n", buf);
                     total_bytes += bytes_read;
                     if (sendto(s, buf, sizeof(buf), 0, (const struct sockaddr *)&clientaddr, clientaddr_len) < 0) // Send the read data over the socket
                     {
-                        error_handler();
+                        error_handler("send error");
                     }
                     bzero(buf, MAX_LINE); // Erase the previous data
-                
-            }
-            printf("Total Bytes sent: %d\n", total_bytes);
-            printf("Loop Count: %d\n", loop_count);
-
-            fclose(fp);
+                }
             }
             else
-            {
-                error_handler();
-            }
+                {
+                    error_handler("write proper get!");
+                }
+
+                printf("Total Bytes sent: %d\n", total_bytes);
+                printf("Loop Count: %d\n", loop_count);
+
+                fclose(fp);
+            
+
             printf("Sending Bye!\n");
             strcpy(buf, "BYE\0");
             if (sendto(s, buf, MAX_LINE - 1, 0, (const struct sockaddr *)&clientaddr, clientaddr_len) < 0)
             {
-                error_handler();
+                error_handler("error sending bye!");
             }
         }
     }
