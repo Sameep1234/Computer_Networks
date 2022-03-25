a07163a7f993514651dd4fe85835a8112125daf9
Sameep1234 <sameep0112vani@gmail.com>
2022-03-25 17:21:42 +0530

    Lab 3 Boiler Code copied

---
+++ Lab3/client/client.c
@@ -0,0 +1,103 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#define SERVER_PORT 8080
+#define MAX_LINE BUFSIZ
+#define IP_ADDRESS "127.0.0.1"
+
+void error_handler(char *error_msg)
+{
+    perror(error_msg);
+    exit(EXIT_FAILURE);
+}
+
+int main(int argc, char *argv[])
+{
+    /* Defining required variables */
+    struct hostent *hp;
+    struct sockaddr_in sin;
+    struct sockaddr_storage servaddr;
+    char *fileName = "sample.mp4";
+    socklen_t servaddr_len = sizeof(struct sockaddr);
+    char buf[MAX_LINE];
+    int s, len, c, r;
+    char *host;
+    FILE *fp;
+    int bytes = 0;
+    char new_buf[MAX_LINE];
+
+    if (argc == 2)
+    {
+        host = argv[1];
+    }
+    else
+    {
+        error_handler("Argument Count Invalid!");
+    }
+    /* Translate host name into peer’s IP address */
+    hp = gethostbyname(host);
+    if (!hp)
+    {
+        error_handler("Host entry Failed!");
+    }
+
+    /* Clearing the memory */
+    bzero((char *)&sin, sizeof(sin));
+
+    /*Prepare the sockaddr_in structure*/
+    sin.sin_family = AF_INET;
+    sin.sin_port = htons(SERVER_PORT);
+
+    /*Copy the address of host in hostent to sockaddr*/
+    bcopy(hp->h_addr, (char *)&sin.sin_addr, hp->h_length);
+
+    /*Creating a socket*/
+    s = socket(PF_INET, SOCK_DGRAM, 0);
+    if (s < 0)
+    {
+        error_handler("Socket creation failure");
+    }
+    puts("Socket created");
+
+    /* Send "GET" directly to receive the video file. */
+    strcpy(new_buf, "GET\0");
+    sendto(s, new_buf, MAX_LINE - 1, 0, (const struct sockaddr *)&sin, (socklen_t)sizeof(struct sockaddr_in));
+
+    /* Open the file in wb mode. */
+    fp = fopen(fileName, "wb");
+    if (NULL == fp)
+    {
+        error_handler("Opening file failed!");
+    }
+
+    int total_bytes = 0, loop_count = 0;
+    /* Receive the data from the server and write it in file. */
+    while (bytes = recvfrom(s, buf, sizeof(buf), 0, (struct sockaddr *)&servaddr, (socklen_t *)&servaddr_len))
+    {
+        total_bytes += bytes;
+        loop_count++;
+        printf("Bytes recieved: %d\n", bytes);
+
+        /* If receive "BYE" means EOF Recieved. */
+        if (strcmp(buf, "BYE") == 0)
+        {
+            printf("EOF Recieved!\n");
+            break;
+        }
+        fwrite(buf, 1, bytes, fp);
+
+        /* Clearing the memory */
+        bzero(buf, MAX_LINE);
+    }
+    printf("Total Bytes Read: %d\n", total_bytes);
+    printf("Loop Count: %d\n", loop_count);
+    /* Close the file. */    
+    fclose(fp);
+    return 0;
+}
---
+++ Lab3/server/server.c
@@ -0,0 +1,121 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <time.h>
+#define SERVER_PORT 8080
+#define MAX_LINE BUFSIZ
+
+/* Error Function */
+void error_handler(char *error_msg)
+{
+    perror(error_msg);
+    exit(EXIT_FAILURE);
+}
+
+int main()
+{
+    /* Defining required variables */
+    struct sockaddr_storage clientaddr;
+    char *fileName = "sample.mp4";
+    socklen_t clientaddr_len = sizeof(clientaddr);
+    struct sockaddr_in sin;
+    char buf[MAX_LINE];
+    int s, new_s, b, len;
+    FILE *fp;
+
+    /* Clearing the memory */
+    bzero((char *)&sin, sizeof(sin));
+
+    /*Prepare the sockaddr_in structure*/
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = inet_addr("127.0.0.1");
+    sin.sin_port = htons(SERVER_PORT);
+
+    /* Creating socket */
+    s = socket(PF_INET, SOCK_DGRAM, 0);
+    if (s < 0)
+    {
+        error_handler("socket creation failure");
+    }
+    puts("Socket created");
+
+    /* Binding the socket */
+    b = bind(s, (struct sockaddr *)&sin, sizeof(sin));
+    if (b < 0)
+    {
+        error_handler("bind error");
+    }
+    puts("Bind done");
+
+    while (1)
+    {
+        int bytes_read = 0, total_bytes, loop_count = 0;
+        /* Receive "GET" from client */
+        while ((len = recvfrom(s, buf, MAX_LINE, 0, (struct sockaddr *)&clientaddr, (socklen_t *)&clientaddr_len)) > 0)
+        {
+            if (strcmp(buf, "GET") == 0)
+            {
+                printf("%s\n", buf);
+                /* Clearing the memory */
+                bzero(buf, sizeof(buf));
+                /* Opening the file in rb mode. */
+                fp = fopen(fileName, "rb");
+                if (fp == NULL)
+                {
+                    error_handler("Opening file failed!");
+                }
+
+                /* Continue the loop until all the bytes of the file is read. */
+                while (!feof(fp))
+                {
+                    /* In our case the optimal delay is 0.001s */
+                    /* 
+                        But to show while transferring we are streaming, 
+                        we chose delay = 0.01s
+                    */
+                    struct timespec t;
+                    t.tv_nsec = 10000000L;
+                    nanosleep((const struct timespec *)&t, NULL);
+
+                    loop_count++;
+                    bytes_read = fread(buf, 1, MAX_LINE - 1, fp);
+                    buf[MAX_LINE - 1] = '\0';
+                    if (ferror(fp) != 0)
+                    {
+                        error_handler("Error in reading file");
+                    }
+                    printf("Bytes Send: %d\n", bytes_read);
+                    total_bytes += bytes_read;
+
+                    /* Send the read data over the socket to client. */
+                    if (sendto(s, buf, bytes_read, 0, (const struct sockaddr *)&clientaddr, clientaddr_len) < 0)
+                    {
+                        error_handler("send error");
+                    }
+                    /* Clearing the memory */
+                    bzero(buf, MAX_LINE);
+                }
+            }
+
+            printf("Total Bytes sent: %d\n", total_bytes);
+            printf("Loop Count: %d\n", loop_count);
+            /* Close the file. */  
+            fclose(fp);
+
+            /* Sending "BYE" to indicate EOF. */
+            printf("Sending Bye!\n");
+            strcpy(buf, "BYE\0");
+            if (sendto(s, buf, MAX_LINE - 1, 0, (const struct sockaddr *)&clientaddr, clientaddr_len) < 0)
+            {
+                error_handler("error sending bye!");
+            }
+        }
+    }
+    return 0;
+}
